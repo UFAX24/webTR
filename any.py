651479be81f00214ad1e0d52e82e5f6b6f8cc90a
UFAX24 <87823194+UFAX24@users.noreply.github.com>
2021-08-01 02:07:44 +0700

    Add files via upload

---
+++ any.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from seoanalyzer import analyze
+
+output = analyze("https://ufax24board.web.app/")
+
+print(output)
---
+++ config.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+MAX_DEPTH = 10  # maximum click depth
+MIN_DEPTH = 3 # minimum click depth
+MAX_WAIT = 10  # maximum amount of time to wait between HTTP requests
+MIN_WAIT = 5  # minimum amount of time allowed between HTTP requests
+DEBUG = True  # set to True to enable useful console output
+
+# use this single item list to test how a site responds to this crawler
+# be sure to comment out the list below it.
+#ROOT_URLS = ["https://ufax24board.web.app/"]
+
+ROOT_URLS = [
+	"https://ufax24board.web.app/",
+	"https://ufax24board.web.app/about",
+	"https://ufax24board.web.app/contact",
+	"https://ufax24board.web.app/faq",
+	"https://ufax24board.web.app/terms",
+	"https://ufax24board.web.app/policy"
+	]
+
+
+# items can be a URL "https://t.co" or simple string to check for "amazon"
+blacklist = [
+	"https://t.co", 
+	"t.umblr.com", 
+	"messenger.com", 
+	"itunes.apple.com", 
+	"l.facebook.com", 
+	"bit.ly", 
+	"mediawiki", 
+	".css", 
+	".ico", 
+	".xml", 
+	"intent/tweet", 
+	"twitter.com/share", 
+	"signup", 
+	"login", 
+	"dialog/feed?", 
+	".png", 
+	".jpg", 
+	".json", 
+	".svg", 
+	".gif", 
+	"zendesk",
+	"clickserve",
+	"https://vk.com/",
+    "https://www.pinterest.com/",
+    "https://www.instagram.com/"
+	]  
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) ' \
+	'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
